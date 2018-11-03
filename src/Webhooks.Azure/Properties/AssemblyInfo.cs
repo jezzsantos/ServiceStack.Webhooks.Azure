d47f1f5e0fc0f7690095b336a35e24481e4c5d8e
Jezz Santos <jezz.santos@hotmail.com>
2018-11-03 21:49:40 +1300

    Removed signed assemblies

--- src/Webhooks.Azure/Properties/AssemblyInfo.cs
+++ src/Webhooks.Azure/Properties/AssemblyInfo.cs
@@ -1,12 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ServiceStack.Webhooks.Azure")]
 [assembly: AssemblyDescription("Webhooks for Azure ServiceStack services")]
 [assembly: Guid("d3918712-67ed-43d1-97cb-7abb963ecaed")]
-
-#if !ASSEMBLYSIGNED
-[assembly:InternalsVisibleTo("ServiceStack.Webhooks.Azure.UnitTests")]
-[assembly:InternalsVisibleTo("ServiceStack.Webhooks.Azure.IntTests")]
-#endif
